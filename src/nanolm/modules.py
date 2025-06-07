f85463ce7d04f28b96845f79201291036cf121e3
Justin Chiu <justinchiu@g.harvard.edu>
2025-06-07 01:54:00 -0400

    mha impl

--- src/nanolm/modules.py
+++ src/nanolm/modules.py
@@ -2,7 +2,7 @@ import torch
 from torch import Tensor, nn
 
 
-class Mha(nn.Module):
+class SlowMha(nn.Module):
     def __init__(self, dim: int, nheads: int, maxseqlen: int, head_dim=128):
         super().__init__()
         self.nheads = nheads
@@ -13,17 +13,22 @@ class Mha(nn.Module):
         self.qkv_proj = nn.Parameter(torch.empty(3, hdim, dim).uniform_(-bound, bound))
         # causal_mask[target, source]: can target attend to source
         self.causal_mask = nn.Buffer(torch.ones(maxseqlen, maxseqlen, dtype=torch.bool))
-        for i in range(0, maxseqlen):
-            self.causal_mask.diagonal(i).add_(-1)
+        for i in range(1, maxseqlen):
+            self.causal_mask.diagonal(i).logical_not_()
+        self.out_proj = nn.Linear(hdim, dim, bias=False)
 
-    def forward(self, x: Tensor) -> Tensor:
-        # x: B x T x D
-        qkv = torch.einsum("btd,shd->btsh", x, self.qkv_proj)
-        q, k, v = qkv.chunk(-2)
-        logits = torch.einsum("bth,bsh->bts", q, k)
-        # mask
-        logits.masked_fill()
-        import pdb; pdb.set_trace()
+    def forward(self, x: Tensor) -> tuple[Tensor, Tensor]:
+        B, T, D = x.shape
+        qkv = torch.einsum("btd,ahd->btah", x, self.qkv_proj)
+        q, k, v = qkv.chunk(3, dim=-2) # leaves singleton split dim
+        q = q.view(B, T, self.nheads, self.head_dim)
+        k = k.view(B, T, self.nheads, self.head_dim)
+        v = v.view(B, T, self.nheads, self.head_dim)
+        logits = torch.einsum("btnh,bsnh->bnts", q, k) * (self.head_dim ** -0.5)
+        masked_logits = logits.masked_fill(self.causal_mask.logical_not(), float("-inf"))
+        attn = masked_logits.softmax(-1)
+        output = torch.einsum("bnts,bsnd->btnd", attn, v)
+        return self.out_proj(output.reshape(B, T, -1)), attn
 
 
 class Ffn(nn.Module):
--- tests/test_modules.py
+++ tests/test_modules.py
@@ -1,15 +1,89 @@
+from dataclasses import dataclass
 import torch
 from torch import nn
-from nanolm.modules import Mha
+from nanolm.modules import SlowMha
+
+
+# reference impl from nanogpt
+@dataclass
+class Config:
+    n_embd: int
+    n_head: int
+    bias: bool = False
+    dropout: float = 0.
+
+
+class CausalSelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+        # regularization
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = config.dropout
+        # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
+        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+        if not self.flash:
+            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
+            # causal mask to ensure that attention is only applied to the left in the input sequence
+            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                        .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        q, k, v  = self.c_attn(x).split(self.n_embd, dim=2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
+
+        # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
+        if self.flash:
+            # efficient attention using Flash Attention CUDA kernels
+            y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
+        else:
+            # manual implementation of attention
+            att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+            att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+            att = F.softmax(att, dim=-1)
+            att = self.attn_dropout(att)
+            y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        # output projection
+        y = self.resid_dropout(self.c_proj(y))
+        return y
 
 
 def test_mha():
-    B,T,D = (2, 4, 3)
+    B,T,D = (2, 4, 8)
     nheads = 2
     x = torch.rand(B,T,D)
 
-    mha_torch = nn.MultiheadAttention(D, nheads)
+    mha_torch = nn.MultiheadAttention(D, nheads, batch_first=True, bias=False)
+    config = Config(D, nheads)
+    mha_nano = CausalSelfAttention(config)
+    mha = SlowMha(D, nheads, T, D // nheads)
 
-    attn_output, attn_weights = mha_torch(x, x, x)
-    import pdb; pdb.set_trace()
+    # convert weights
+    mha_torch.in_proj_weight.data.copy_(mha.qkv_proj.data.view_as(mha_torch.in_proj_weight.data))
+    mha_torch.out_proj.weight.data.copy_(mha.out_proj.weight.data)
+    mha_nano.c_attn.weight.data.copy_(mha.qkv_proj.data.reshape(3*D, D))
+    mha_nano.c_proj.weight.data.copy_(mha.out_proj.weight.data)
+
+    attn_output, attn_weights = mha_torch(x, x, x, attn_mask=mha.causal_mask.logical_not(), is_causal=True, average_attn_weights=False)
+    nano_output = mha_nano(x)
+    output, weights = mha(x)
+
+    assert torch.allclose(attn_weights, weights)
+    assert torch.allclose(attn_output, output)
+    assert torch.allclose(nano_output, output)
 
