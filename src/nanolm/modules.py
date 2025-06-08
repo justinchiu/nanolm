8f831edb6ad4da0dd6b2c6076cf036fba63aea41
Justin Chiu <justinchiu@g.harvard.edu>
2025-06-08 01:25:46 -0400

    finish transformer

--- src/nanolm/modules.py
+++ src/nanolm/modules.py
@@ -1,14 +1,29 @@
 import torch
 from torch import Tensor, nn
+import torch.nn.functional as F
+
 
 class Rotary(nn.Module):
-    def __init__(self, dim: int, maxseqlen: int):
+    def __init__(self, dim: int, maxseqlen: int, theta: float = 10_000):
         super().__init__()
         self.dim = dim
         self.maxseqlen = maxseqlen
+        # angular speed
+        speed = theta ** -(torch.arange(0, dim, 2)[:dim//2].float() / dim)
+        t = torch.arange(maxseqlen)
+        # rotations at each timestep
+        freqs = torch.outer(t, speed)
+        self.freqs_cis = torch.polar(torch.ones_like(freqs), freqs)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Apply rotations at each timesteps"""
+        # batch x time x nheads x head_dim
+        xc = torch.view_as_complex(x.view(*x.shape[:-1], -1, 2))
+        return torch.view_as_real(xc * self.freqs_cis[None,:,None,:]).flatten(-2)
 
 
 class SlowMha(nn.Module):
+    """Self attention only"""
     def __init__(self, dim: int, nheads: int, maxseqlen: int, head_dim=128):
         super().__init__()
         self.nheads = nheads
@@ -17,6 +32,7 @@ class SlowMha(nn.Module):
         std = 0.5 * (dim ** -0.5)
         bound = (3 ** 0.5) * std
         self.qkv_proj = nn.Parameter(torch.empty(3, hdim, dim).uniform_(-bound, bound))
+        self.rotary = Rotary(dim, maxseqlen)
         # causal_mask[target, source]: can target attend to source
         # should be lower triangular
         self.causal_mask = nn.Buffer(torch.ones(maxseqlen, maxseqlen, dtype=torch.bool))
@@ -28,6 +44,8 @@ class SlowMha(nn.Module):
         B, T, D = x.shape
         qkv = torch.einsum("btd,ahd->btah", x, self.qkv_proj)
         q, k, v = qkv.view(B, T, 3*self.nheads, self.head_dim).chunk(3, dim=-2)
+        q = self.rotary(q)
+        k = self.rotary(k)
         logits = torch.einsum("btnh,bsnh->bnts", q, k) * (self.head_dim ** -0.5)
         masked_logits = logits.masked_fill(self.causal_mask.logical_not(), float("-inf"))
         attn = masked_logits.softmax(-1)
@@ -51,3 +69,43 @@ class Ffn(nn.Module):
         x = self.proj2(x)
         return x
 
+
+def norm(x: Tensor) -> Tensor:
+    return F.rmsnorm(x, (x.shape[-1],))
+
+
+class Block(nn.Module):
+    def __init__(self, dim: int, nheads: int, maxseqlen: int):
+        super().__init__()
+        self.mha = SlowMha(dim, nheads, maxseqlen, dim)
+        self.ffn = Ffn(dim)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = x + self.mha(x)[0]
+        x = x + self.ffn(x)
+        return x
+
+
+class Transformer(nn.Module):
+    def __init__(self, vocab: int, nblocks: int, dim: int, nheads: int, maxseqlen: int):
+        super().__init__()
+        self.emb = nn.Embedding(vocab, dim)
+        self.blocks = nn.ModuleList([Block(dim, nheads, maxseqlen) for _ in range(nblocks)])
+        self.output_proj = nn.Linear(dim, vocab, bias=False)
+        self.output_proj.weight.data.zero_()
+
+    def forward(self, input_seq: Tensor, target_seq: Tensor) -> Tensor:
+        x = self.emb(input_seq)
+        for block in self.blocks:
+            x = block(x)
+        x = self.output_proj(x).log_softmax(-1)
+        x = x.log_softmax(-1)
+        logprobs = x.gather(-1, target_seq[:,:,None]).squeeze()
+        # lp = F.cross_entropy(x.view(-1, x.shape[-1]), target_seq.view(-1), reduction="none")
+        # assert torch.allclose(logprobs, lp)
+        return logprobs
+
+
+if __name__ == "__main__":
+    transformer = Transformer(128, 2, 8, 2, 5)
+    transformer(torch.arange(5)[None], torch.arange(1, 6)[None])
--- tests/test_modules.py
+++ tests/test_modules.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import torch
-from torch import nn
-from nanolm.modules import SlowMha
+from torch import nn, Tensor
+from nanolm.modules import SlowMha, Rotary
 
 
 # reference impl from nanogpt
@@ -62,6 +62,82 @@ class CausalSelfAttention(nn.Module):
         y = self.resid_dropout(self.c_proj(y))
         return y
 
+# llama rotary embeddings
+def precompute_freqs_cis(dim: int, end: int, theta: float = 10000.0):
+    """
+    Precompute the frequency tensor for complex exponentials (cis) with given dimensions.
+
+    This function calculates a frequency tensor with complex exponentials using the given dimension 'dim'
+    and the end index 'end'. The 'theta' parameter scales the frequencies.
+    The returned tensor contains complex values in complex64 data type.
+
+    Args:
+        dim (int): Dimension of the frequency tensor.
+        end (int): End index for precomputing frequencies.
+        theta (float, optional): Scaling factor for frequency computation. Defaults to 10000.0.
+
+    Returns:
+        torch.Tensor: Precomputed frequency tensor with complex exponentials.
+    """
+    freqs = 1.0 / (theta ** (torch.arange(0, dim, 2)[: (dim // 2)].float() / dim))
+    t = torch.arange(end, device=freqs.device)  # type: ignore
+    freqs = torch.outer(t, freqs).float()  # type: ignore
+    freqs_cis = torch.polar(torch.ones_like(freqs), freqs)  # complex64
+    return freqs_cis
+
+
+def reshape_for_broadcast(freqs_cis: Tensor, x: Tensor):
+    """
+    Reshape frequency tensor for broadcasting it with another tensor.
+
+    This function reshapes the frequency tensor to have the same shape as the target tensor 'x'
+    for the purpose of broadcasting the frequency tensor during element-wise operations.
+
+    Args:
+        freqs_cis (torch.Tensor): Frequency tensor to be reshaped.
+        x (torch.Tensor): Target tensor for broadcasting compatibility.
+
+    Returns:
+        torch.Tensor: Reshaped frequency tensor.
+
+    Raises:
+        AssertionError: If the frequency tensor doesn't match the expected shape.
+        AssertionError: If the target tensor 'x' doesn't have the expected number of dimensions.
+    """
+    ndim = x.ndim
+    assert 0 <= 1 < ndim
+    assert freqs_cis.shape == (x.shape[1], x.shape[-1])
+    shape = [d if i == 1 or i == ndim - 1 else 1 for i, d in enumerate(x.shape)]
+    return freqs_cis.view(*shape)
+
+
+def apply_rotary_emb(
+    xq: Tensor,
+    xk: Tensor,
+    freqs_cis: Tensor,
+) -> tuple[Tensor, Tensor]:
+    """
+    Apply rotary embeddings to input tensors using the given frequency tensor.
+
+    This function applies rotary embeddings to the given query 'xq' and key 'xk' tensors using the provided
+    frequency tensor 'freqs_cis'. The input tensors are reshaped as complex numbers, and the frequency tensor
+    is reshaped for broadcasting compatibility. The resulting tensors contain rotary embeddings and are
+    returned as real tensors.
+
+    Args:
+        xq (torch.Tensor): Query tensor to apply rotary embeddings.
+        xk (torch.Tensor): Key tensor to apply rotary embeddings.
+        freqs_cis (torch.Tensor): Precomputed frequency tensor for complex exponentials.
+
+    Returns:
+        Tuple[torch.Tensor, torch.Tensor]: Tuple of modified query tensor and key tensor with rotary embeddings.
+    """
+    xq_ = torch.view_as_complex(xq.float().reshape(*xq.shape[:-1], -1, 2))
+    xk_ = torch.view_as_complex(xk.float().reshape(*xk.shape[:-1], -1, 2))
+    freqs_cis = reshape_for_broadcast(freqs_cis, xq_)
+    xq_out = torch.view_as_real(xq_ * freqs_cis).flatten(3)
+    xk_out = torch.view_as_real(xk_ * freqs_cis).flatten(3)
+    return xq_out.type_as(xq), xk_out.type_as(xk)
 
 def test_mha():
     B,T,D = (2, 4, 8)
@@ -87,3 +163,11 @@ def test_mha():
     assert torch.allclose(attn_output, output)
     assert torch.allclose(nano_output, output)
 
+def test_rotary():
+    B,T,H,D = (5, 6, 2, 8)
+    x = torch.ones(B, T, H, D)
+    rotary = Rotary(D, T)
+    y = rotary(x)
+    freqs_cis = precompute_freqs_cis(D, T)
+    y_llama, _ = apply_rotary_emb(x, x, freqs_cis=freqs_cis)
+    assert torch.allclose(y, y_llama)
